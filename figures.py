a572eda124d386a0406ac659ade53a2e9a5ae3f8
Артём Габерман <artemgaberman@gmail.com>
2025-02-09 17:58:10 +0700

    Update figures.py

--- figures.py
+++ figures.py
@@ -1,2 +1,10 @@
-import turtle
+import turtle as ttl
+#Main_zone:
+#Square()квадрат)
+#Isosceless_triangle(равнобедренный треугольник)
+#Right_triangle(прямой треугольник)
+#parallelogram
+#rhombus
+#rectangle(прямоугольник)
+#trapezoid
 
