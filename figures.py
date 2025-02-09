46561cc9895baf697eae6edfb9b828e1f89d1957
Артём Габерман <artemgaberman@gmail.com>
2025-02-09 19:25:27 +0700

    Update figures.py

--- figures.py
+++ figures.py
@@ -10,6 +10,8 @@ ttl.forward(250)
 ttl.right(90)
 
 #rectangle(прямоугольник)
+ttl.fillcolor('red')
+ttl.begin_fill()
 ttl.forward(75)
 ttl.right(90)
 ttl.forward(150)
@@ -17,15 +19,20 @@ ttl.right(90)
 ttl.forward(75)
 ttl.right(90)
 ttl.forward(150)
+ttl.end_fill()
 
 #trapezoid
 ttl.right(90)
 ttl.forward(75)#точка границы с прямоугольником
+ttl.fillcolor('blue')
+ttl.begin_fill()
 ttl.forward(140)#левая грань трапеции
 ttl.right(90)
 ttl.forward(99)
 ttl.right(70)
 ttl.forward(149)
+ttl.end_fill()
+turtle.done()
 
 #Square()квадрат)
 #Isosceless_triangle(равнобедренный треугольник)
