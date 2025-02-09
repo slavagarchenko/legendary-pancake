2ec93b44dcf982f89f2ac5c579362927998f74f6
Артём Габерман <artemgaberman@gmail.com>
2025-02-09 19:08:11 +0700

    Update figures.py

--- figures.py
+++ figures.py
@@ -1,10 +1,36 @@
 import turtle as ttl
 #Main_zone:
+ttl.forward(250)
+ttl.right(90)
+ttl.forward(250)
+ttl.right(90)
+ttl.forward(250)
+ttl.right(90)
+ttl.forward(250)
+ttl.right(90)
+
+#rectangle(прямоугольник)
+ttl.forward(75)
+ttl.right(90)
+ttl.forward(150)
+ttl.right(90)
+ttl.forward(75)
+ttl.right(90)
+ttl.forward(150)
+
+#trapezoid
+ttl.right(90)
+ttl.forward(75)#точка границы с прямоугольником
+ttl.forward(140)#левая грань трапеции
+ttl.right(90)
+ttl.forward(99)
+ttl.right(70)
+ttl.forward(149)
+
 #Square()квадрат)
 #Isosceless_triangle(равнобедренный треугольник)
 #Right_triangle(прямой треугольник)
 #parallelogram
 #rhombus
-#rectangle(прямоугольник)
-#trapezoid
+
 
